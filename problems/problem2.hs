183f3eb86d1c7140096799481635d4aba9511222
Bob Brahms <bob.brahms@gmail.com>
2022-01-13 17:14:10 -0500

    every third fib is even

--- problems/problem2.hs
+++ problems/problem2.hs
@@ -3,9 +3,21 @@
 -- Find the sum of all even terms of fibonacci sequence below 4 million.
 
 fibsForever ::  [Integer]
-fibsForever = 1:2:(fibs 1 2)
+fibsForever = 1:1:2:(fibs 1 2)
   where
     fibs p l = (p + l) : fibs l (p+l)
 
 sum [x | x <- takeWhile (<4000000) (fibsForever), mod x 2 == 0]
+
+-- some interesting properties observed in the discussion.
+-- https://projecteuler.net/overview=002
+-- one of which being, every third fib is even.
+
+everyThird :: [Integer] -> [Integer]
+everyThird (_:_:it:theRest) =
+  it : everyThird theRest
+
+-- so this also works
+sum [x | x <- takeWhile (<4000000) (everyThird fibsForever)]
+
 
